d83f3f18ddfeb0f3d94e0ca47d388bb95dfa62e7
Raphael Farias <farias.raphael@gmail.com>
2024-03-02 17:30:06 -0300

    updates

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,33 @@
+// external modules
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
 
+// internal modules
+import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+// State and reducers
+import flatsReducer from './reducers/flats_reducer';
+import selectedFlatReducer from './reducers/selected_flat_reducer'
 
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+const reducers = combineReducers({
+  //changeMe: (state = null, action) => state
+  flats: flatsReducer,
+  selectedFlat: selectedFlatReducer
+});
+
+function mapReduxStateToProps(reduxState) {
+  return {
+    flat: reduxState.flats
+  };
 }
+
+// render an instance of the component in the DOM
+const root = document.getElementById('root')
+createRoot(root).render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>
+);
